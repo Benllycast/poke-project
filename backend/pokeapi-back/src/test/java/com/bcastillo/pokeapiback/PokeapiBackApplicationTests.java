54bfcf27087a5e4f871041dca14993ef3c524985
Benlly <benllycast@gmail.com>
2026-08-15 17:29:25 -0500

    adding backend files

---
+++ backend/pokeapi-back/src/test/java/com/bcastillo/pokeapiback/PokeapiBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcastillo.pokeapiback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeapiBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
